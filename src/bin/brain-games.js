0c3ae01e39e6b9d553638762cb81ae4f1831d416
Pavel Egorov MrFSP <epe.m@mail.ru>
2019-12-01 17:21:19 +0300

    add & change files
    change brain-games.js
    add index.js
    
    Result: "brain-games" programm could greet users.

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,3 +1,9 @@
 #!/usr/bin/env node
 
-console.log('Welcome to the Brain Games!');
+import { name } from '..';
+
+console.log('Welcome to the Brain Games!\n');
+
+const userName = name();
+
+console.log(`Hello, ${userName}!`);
