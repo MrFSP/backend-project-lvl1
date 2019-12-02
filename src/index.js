497a4581e2867865f95bfb3964351c7548df146f
Pavel Egorov MrFSP <epe.m@mail.ru>
2019-12-03 00:23:37 +0300

    refactor(brain-games.js brain-even.js index.js)
    modify
    - src/bin/brain-even.js
    - src/bin/brain-games.js
    - src/index.js
    - package.json

--- src/index.js
+++ src/index.js
@@ -4,41 +4,38 @@ const greeting = 'Welcome to the Brain Games!\n';
 
 const name = () => readlineSync.question('May I have your name? ');
 
-const helloUser = (name) => console.log(`Hello, ${name}!\n`);
+const helloUser = (userName) => console.log(`Hello, ${userName}!\n`);
 
 // Random number
-const getRandomInt = (min, max) => {
-	return Math.floor(Math.random() * (max - min + 1)) + min;
-};
+const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 const makeNumber = () => {
-    const number = getRandomInt(0, 100);
-    console.log(`Question: ${number}`);
-    return number % 2 === 0 ? 'yes' : 'no';
+  const number = getRandomInt(0, 100);
+  console.log(`Question: ${number}`);
+  return number % 2 === 0 ? 'yes' : 'no';
 };
 
 export const brainEven = () => {
-    console.log(`${greeting}Answer "yes" if number even otherwise answer "no".\n`);
-    const userName = name();
-    helloUser(userName);
-    for(let i = 0; i <= 3; i += 1) {
-        if(i === 3) {
-            console.log(`Congratulations, ${userName}!\n`);
-            break;
-        }
-        const correctAnswer = makeNumber();
-        const userAnswer = readlineSync.question('Your answer: ');
-        if(userAnswer === correctAnswer) {
-            console.log('Correct!\n');
-        }
-        else {
-            console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${correctAnswer}".\nLet's try again, ${userName}!\n`);
-            break;
-        }
+  console.log(`${greeting}Answer "yes" if number even otherwise answer "no".\n`);
+  const userName = name();
+  helloUser(userName);
+  for (let i = 0; i <= 3; i += 1) {
+    if (i === 3) {
+      console.log(`Congratulations, ${userName}!\n`);
+      break;
     }
-}; 
+    const correctAnswer = makeNumber();
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (userAnswer === correctAnswer) {
+      console.log('Correct!\n');
+    } else {
+      console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${correctAnswer}".\nLet's try again, ${userName}!\n`);
+      break;
+    }
+  }
+};
 
 export const brainGames = () => {
-    console.log(greeting);
-    const userName = name();
-    helloUser(userName);
+  console.log(greeting);
+  const userName = name();
+  helloUser(userName);
 };
