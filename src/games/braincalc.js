429c83786d4c44924064f72e1aa0069c4d2e95ee
Pavel Egorov MrFSP <epe.m@mail.ru>
2019-12-06 16:49:56 +0300

    refactor(braincalc.js)

--- src/games/braincalc.js
+++ src/games/braincalc.js
@@ -3,21 +3,23 @@ import { getRandomInt } from './functions';
 export default () => {
   const getTypeOfOperator = getRandomInt(1, 3);
   let answer = 0;
-  const number1 = getRandomInt(0, 100);
-  const number2 = getTypeOfOperator === 3 ? getRandomInt(0, 10) : getRandomInt(0, 100);
+  const firstMember = getRandomInt(0, 100); // First member for all expressions
+  // eslint-disable-next-line max-len
+  const secondMember = getRandomInt(0, 100); // Second member for all expressions (except multiplication)
+  const multiplier = getRandomInt(0, 10); // Second member for multiplication
 
   switch (getTypeOfOperator) {
     case 1:
-      console.log(`Question: ${number1} + ${number2}`);
-      answer = number1 + number2;
+      console.log(`Question: ${firstMember} + ${secondMember}`);
+      answer = firstMember + secondMember;
       break;
     case 2:
-      console.log(`Question: ${number1} - ${number2}`);
-      answer = number1 - number2;
+      console.log(`Question: ${firstMember} - ${secondMember}`);
+      answer = firstMember - secondMember;
       break;
     case 3:
-      console.log(`Question: ${number1} * ${number2}`);
-      answer = number1 * number2;
+      console.log(`Question: ${firstMember} * ${multiplier}`);
+      answer = firstMember * multiplier;
       break;
     default:
   }
