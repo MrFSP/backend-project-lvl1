c5ca1af0559b73d4384e85e62edea85e1af7823d
Pavel Egorov MrFSP <epe.m@mail.ru>
2019-12-27 15:35:56 +0300

    refactor

--- src/games/prime.js
+++ src/games/prime.js
@@ -13,26 +13,29 @@ const isPrime = (number) => {
   return true;
 };
 // Get random simple number on the segment [2, max]
-const getRanSimpleNum = (max) => {
-  let answer = getRandomInt(3, max);
-  while (isPrime(answer) === true) {
-    answer = getRandomInt(3, max);
-  }
-  return answer;
-};
-
-// Get random composite number on the segment [2, max]
-const getRanCompNum = (max) => {
-  let answer = getRandomInt(2, max);
-  while (isPrime(answer) !== true) {
-    answer = getRandomInt(2, max);
+const getRanNum = (str, max) => {
+  let answer;
+  // eslint-disable-next-line default-case
+  switch (str) {
+    case 'simple':
+      answer = getRandomInt(3, max);
+      while (isPrime(answer) === true) {
+        answer = getRandomInt(3, max);
+      }
+      break;
+    case 'complex':
+      answer = getRandomInt(2, max);
+      while (isPrime(answer) !== true) {
+        answer = getRandomInt(2, max);
+      }
+      break;
   }
   return answer;
 };
 
 const prime = () => {
   const isPrimeForGame = getRandomInt(0, 1) === 1;
-  const number = isPrimeForGame ? getRanSimpleNum(maxNum) : getRanCompNum(maxNum);
+  const number = isPrimeForGame ? getRanNum('simple', maxNum) : getRanNum('complex', maxNum);
   const question = number;
   const answer = isPrime(question) === true ? 'yes' : 'no';
   return { question, answer };
